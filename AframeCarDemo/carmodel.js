bc322083c4b57b138d8d4a39a07846b4097e0194
pooja201095 <poojachavan153@gmail.com>
2019-05-23 16:15:38 +0530

    Add files via upload

---
+++ AframeCarDemo/carmodel.js
@@ -0,0 +1,26 @@
+ AFRAME.registerComponent('mythreejsthing',{
+            schema: {
+                color: {
+                    default: '#000'
+                },
+            },
+                init: function () {
+                    var el = document.querySelector('myapp');
+                    var camera = document.querySelector('a-camera').components.camera.camera;
+                    camera.position.z = 5;
+                    camera.position.y = -3;
+                    var loader = new THREE.ObjectLoader();
+                    var that = this;
+                    loader.load(
+                        // resource URL
+                        "car.json",
+
+                        // onLoad callback
+                        // Here the loaded data is assumed to be an object
+                        function (obj) {
+                            // Add the loaded object to the scene
+                            that.el.setObject3D('obj', obj);
+                        }
+                    );
+                }
+            });
